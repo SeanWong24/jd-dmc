bbdf37f9be47745ba117b3f3e5b733821580fe73
Sean Wong <seanwong24@live.com>
2025-09-01 01:05:07 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { setupApp } from "https://git.foo-bar.top/sean/deno-micro-cms/raw/branch/main/mod.ts";
+
+const app = await setupApp({
+  STATIC_ROUTES: `/admin,www-admin,index.html,index.html:/,www,index.html,index.html`,
+});
+
+export default {
+  fetch: app.fetch,
+};
