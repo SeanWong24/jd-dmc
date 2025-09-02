5db8093850f2e2de1aef65d845b4bbebd31e7d8b
Sean Wong <seanwong24@live.com>
2025-09-01 17:42:07 -0700

    update

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { setupApp } from "https://git.foo-bar.top/sean/deno-micro-cms/raw/commit/5994edefd167765fb2caef800334fe848f260892/mod.ts";
+import { setupApp } from "https://git.foo-bar.top/sean/deno-micro-cms/raw/commit/efa4105079f6460f912ca4b36462d2aabcf16923/mod.ts";
 
 const app = await setupApp({
   STATIC_ROUTES: `/admin,www-admin,index.html,index.html;,www,index.html,index.html`,
